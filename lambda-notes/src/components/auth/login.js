bfa921664a759177f46da184967073b1cc78abc5
harrisonbrock <harrison@harrisonbrock.com>
2018-05-30 11:57:11 -0400

    Added react router Added the start of register and login components

---
+++ lambda-notes/src/components/auth/login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Login extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Register Component</h1>
+            </div>
+        );
+    }
+}
+
+export default Login;
---
+++ lambda-notes/src/components/auth/register.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Register extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Register Component</h1>
+            </div>
+        );
+    }
+}
+
+export default Register
