49d09856d959139bdc7fe5a384224e05bc74aa94
harrisonbrock <harrison@harrisonbrock.com>
2018-06-01 11:15:05 -0400

    Added logic to register user.

--- src/components/auth/register.js
+++ src/components/auth/register.js
@@ -1,16 +1,40 @@
 import React from 'react';
-
+import axios from 'axios';
+import keys from '../../config/keys';
 class Register extends React.Component {
     constructor() {
         super();
         this.state = {
             name: '',
             email: '',
-            password: '',
-            password2: ''
+            password: ''
         }
     }
 
+    inputChangeHandler = event => {
+
+        event.preventDefault();
+        const {name, value} = event.target;
+        this.setState({[name]: value});
+
+    };
+
+    submitHandler = event => {
+
+        event.preventDefault();
+        axios
+            .post(keys.userRegister, this.state)
+            .then(response => {
+                console.log(response.data);
+                this.props.history.push('/login');
+            })
+            .catch(err => {
+
+                console.log(`Error signing up user ${err}`);
+
+            });
+
+    };
     render() {
         return (
             <div className="register">
@@ -21,12 +45,13 @@ class Register extends React.Component {
                             <p className="lead text-center">
                                 Create your Lambda Note account
                             </p>
-                            <form noValidate onSubmit={this.onSubmit}>
+                            <form noValidate onSubmit={this.submitHandler}>
                                 <div className="form-group">
                                     <input
                                         type="text"
                                         placeholder="Name"
                                         name="name"
+                                        onChange={this.inputChangeHandler}
                                     />
                                 </div>
                                 <div className="form-group">
@@ -34,6 +59,7 @@ class Register extends React.Component {
                                         type="email"
                                         placeholder="Email Address"
                                         name="email"
+                                        onChange={this.inputChangeHandler}
                                     />
                                     <small className="form-text text-muted">
                                         This site uses Gravatar so if you want a profile image, use
@@ -45,13 +71,7 @@ class Register extends React.Component {
                                         type="password"
                                         placeholder="Password"
                                         name="password"
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <input
-                                        type="password"
-                                        placeholder="Confirm Password"
-                                        name="password2"
+                                        onChange={this.inputChangeHandler}
                                     />
                                 </div>
                                 <button className="btn btn-info btn-block mt-4">Create Account</button>
